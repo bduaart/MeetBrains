c2f03b43efb53aa4e9bb4ce5c48c154e501ba4bc
Bruno Duarte <contato.bduaart@gmail.com>
2022-01-04 19:11:53 -0300

    Create admin validation

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { CreateUserController } from "../controllers/CreateUserController";
 import { CreateEventController } from "../controllers/CreateEventController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 const router = Router();
 const createUserController = new CreateUserController();
 const createEventController = new CreateEventController();
 
 router.post("/users", createUserController.handle);
-router.post("/events", createEventController.handle);
+router.post("/events", ensureAdmin, createEventController.handle);
 
 export { router }
